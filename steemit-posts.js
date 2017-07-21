aad36fabb17c14d639ce3d6ce0d9ce1316e4d2f4
Markus <kontakt@markus-kottlaender.de>
2017-07-21 21:59:59 +0200

    implemented resteemed indicator

--- steemit-posts.js
+++ steemit-posts.js
@@ -5,8 +5,9 @@
         var settings = $.extend({
             user: "mkt",
             limit: 10,
-            postTemplate: '<a class="steemit-post" href="https://steemit.com${URL}"><h2>${TITLE}</h2><h5>${DATE}</h5><img src="${IMAGE}" alt="${TITLE}" /><p>${PAYOUT}</p><p>${COMMENTS} comments</p><p>${UPVOTES} upvotes</p></a>',
+            postTemplate: '<a class="steemit-post" href="https://steemit.com${URL}"><h2>${RESTEEMED}${TITLE}</h2><h5>${DATE}</h5><img src="${IMAGE}" alt="${TITLE}" /><p>${PAYOUT}</p><p>${COMMENTS} comments</p><p>${UPVOTES} upvotes</p></a>',
             defaultImage: 'http://via.placeholder.com/860x460',
+            resteemedHtml: 'Resteemed: ',
             dateCallback: function (date) {
                 return date;
             }
@@ -20,6 +21,7 @@
                 var html = settings.postTemplate
                     .replace(/\${URL}/gi, 'https://steemit.com' + blog[i].url)
                     .replace(/\${TITLE}/gi, blog[i].title)
+                    .replace(/\${RESTEEMED}/gi, blog[i].author != settings.user ? settings.resteemedHtml : '')
                     .replace(/\${DATE}/gi, settings.dateCallback(new Date(blog[i].created)))
                     .replace(/\${IMAGE}/gi, metaData.image ? metaData.image[0] : settings.defaultImage)
                     .replace(/\${PAYOUT}/gi, blog[i].pending_payout_value)
